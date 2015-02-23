1af411cffc9a284decaeab482ba425f6c7665061
Jeremy Quentin <jeremy.quentin@gmail.com>
2015-02-23 00:43:03 -0500

    level selection

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButton : MonoBehaviour {
+
+	public int level;
+
+	IEnumerator OnMouseDown () 
+	{
+		transform.position = new Vector3(transform.position.x, transform.position.y, -1f);
+		ScaleTweenable st = ScaleTweenable.Get(gameObject);
+		st.TweenScale(4f * st.scale, 1f);
+		PositionTweenable.Get(gameObject).TweenYPosition(0f, 1f);
+		yield return new WaitForSeconds(1.1f);
+		Application.LoadLevel(level);
+	}
+
+}
